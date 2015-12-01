700980c3acaf22b294508d40ac9da04aed7600be
Mykhailo Danylov <mike.danylov@gmail.com>
2015-12-02 00:07:48 +0200

    Done hw1

--- chapter1/interface.js
+++ chapter1/interface.js
@@ -3,7 +3,15 @@
  */
 exports.insert = function(db, doc, callback) {
   // TODO: implement
-  callback(null);
+  db.collection('movies').insert(doc, function(error, result) {
+    if (error) {
+      console.log('Error has occured: ' + error);
+      process.exit(1);
+    } else {
+      console.log('Success: ' + result);
+      callback(null, result);
+    }
+  });
 };
 
 /*
@@ -14,5 +22,17 @@ exports.insert = function(db, doc, callback) {
  */
 exports.byDirector = function(db, director, callback) {
   // TODO: implement
-  callback(null, []);
+  db.collection('movies').find({'director': director})
+    .sort({title: 1}).toArray(function(error, docs) {
+      if (error){
+        console.log('Error has occured: ' + error);
+        process.exit(1);
+      } else {
+        console.log('Found docs:');
+        docs.forEach(function(doc) {
+          console.log(JSON.stringify(doc));
+        });
+        callback(null, docs);
+      }
+  });
 };
